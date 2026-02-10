157d3a04c61165f59460c57b1a754da144fc6171
Hiroshiyz <yeven1102@gmail.com>
2026-02-10 17:10:19 +0800

    add new hashRpouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { APIProvider } from "@vis.gl/react-google-maps";
 import MainLayout from "./layouts/MainLayout"; // 匯入拆分後的 Layout
 
@@ -8,12 +8,12 @@ function App() {
       apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}
       libraries={["places"]}
     >
-      <BrowserRouter basename="travel-web-app">
+      <HashRouter>
         <Routes>
           {/* 所有邏輯都在 MainLayout 處理 */}
           <Route path="/" element={<MainLayout />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </APIProvider>
   );
 }
